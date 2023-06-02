151aa978e09a3d56cc2b7cc4fba3cde5f34a59fc
Tianyu Chen <tianyu.chen@jax.org>
2023-06-02 15:13:15 -0700

    Batch files added

---
+++ IMPC.bat
@@ -0,0 +1,26 @@
+@echo off
+
+ECHO Populating experiments on lines table
+set VAR_1="IMPC"
+set VAR_2="-l"
+python src\App.py %VAR_1% %VAR_2%
+ECHO Job done
+
+timeout 5 > NUL
+
+ECHO Populating experiments on specimens table
+set VAR_1="IMPC"
+set VAR_2="-e"
+python src\App.py %VAR_1% %VAR_2%
+ECHO Job done
+
+timeout 5 > NUL
+
+ECHO Populating images table
+set VAR_1="IMPC"
+set VAR_2="-i"
+python src\App.py %VAR_1% %VAR_2%
+ECHO Job done
+
+ECHO All processes finished
+@pause
